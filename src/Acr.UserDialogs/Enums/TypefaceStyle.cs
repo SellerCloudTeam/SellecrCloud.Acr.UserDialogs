8a1cd8fa306267bbcb67e75b8b14301ea8207481
Dimitar.Chobanov <chobanov.d@sellercloud.com>
2019-08-22 17:52:49 +0300

    Initial commit.

---
+++ src/Acr.UserDialogs/Enums/TypefaceStyle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Acr.UserDialogs.Enums
+{
+    public enum TypefaceStyle
+    {
+        Normal = 0,
+        Bold = 1,
+        Italic = 2,
+        BoldItalic = 3
+    }
+}
---
+++ src/Acr.UserDialogs/SCToastConfig.cs
@@ -0,0 +1,45 @@
+﻿using Acr.UserDialogs.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Acr.UserDialogs
+{
+    public class SCToastConfig : ToastConfig
+    {
+        public SCToastConfig(
+            string message
+            , int leftMargin
+            , int topMargin
+            , int rightMargin
+            , int bottomMargin)
+            : base(message)
+        {
+            this.LeftMargin = leftMargin;
+            this.TopMargin = topMargin;
+            this.RightMargin = rightMargin;
+            this.BottomMargin = bottomMargin;
+        }
+
+        public int LeftMargin { get; set; }
+        public int TopMargin { get; set; }
+        public int RightMargin { get; set; }
+        public int BottomMargin { get; set; }
+
+        public int FontSize { get; set; }
+
+        public TypefaceStyle TextStyle { get; set; }
+
+        public SCToastConfig SetTextStyle(TypefaceStyle textStyle)
+        {
+            this.TextStyle = textStyle;
+            return this;
+        }
+
+        public SCToastConfig SetFontSize(int size)
+        {
+            this.FontSize = size;
+            return this;
+        }
+    }
+}
